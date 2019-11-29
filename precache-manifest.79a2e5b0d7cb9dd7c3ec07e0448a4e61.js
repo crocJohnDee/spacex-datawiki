362440b2bf748b1e0ee34aa9294ac95c8937c7cb
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-11-29 10:32:11 +0100

    Updates

---
+++ precache-manifest.79a2e5b0d7cb9dd7c3ec07e0448a4e61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bb3d50a5116e8f211f4324409eb3e1",
+    "url": "/spacex-datawiki/index.html"
+  },
+  {
+    "revision": "970c03696a205ff4f573",
+    "url": "/spacex-datawiki/static/css/2.5f5aa264.chunk.css"
+  },
+  {
+    "revision": "e82f63188bd55266be9d",
+    "url": "/spacex-datawiki/static/css/main.34c778c4.chunk.css"
+  },
+  {
+    "revision": "970c03696a205ff4f573",
+    "url": "/spacex-datawiki/static/js/2.4dbe3976.chunk.js"
+  },
+  {
+    "revision": "e82f63188bd55266be9d",
+    "url": "/spacex-datawiki/static/js/main.b98fa846.chunk.js"
+  },
+  {
+    "revision": "3b33460be6eefecb8b60",
+    "url": "/spacex-datawiki/static/js/runtime-main.c1bbf77a.js"
+  },
+  {
+    "revision": "d6f223c893b486aebe07146d7f18ccf1",
+    "url": "/spacex-datawiki/static/media/bg.d6f223c8.jpg"
+  },
+  {
+    "revision": "1c231956146b2099a987002d6716f752",
+    "url": "/spacex-datawiki/static/media/noimg.1c231956.jpg"
+  }
+]);
